c0d5f41a628526280a8cbe90793b917b0d257eb6
ritik-cyber <ritikdhingra03@gmail.com>
2021-05-29 09:50:26 +0530

    done

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -1,27 +1,27 @@
 let movies = [
   {
-    id: '1',
-    title: 'Game Night',
-    year: '2018',
-    genres: ['Action', 'Comedy', 'Crime'],
-    releaseDate: '2018-02-28',
-    actors: ['Rachel McAdams', 'Jesse Plemons', 'Jason Bateman'],
+    id: "1",
+    title: "Game Night",
+    year: "2018",
+    genres: ["Action", "Comedy", "Crime"],
+    releaseDate: "2018-02-28",
+    actors: ["Rachel McAdams", "Jesse Plemons", "Jason Bateman"],
   },
   {
-    id: '2',
-    title: 'Area X: Annihilation',
-    year: '2018',
-    genres: ['Adventure', 'Drama', 'Fantasy'],
-    releaseDate: '2018-02-23',
-    actors: ['Tessa Thompson', 'Jennifer Jason Leigh', 'Natalie Portman'],
+    id: "2",
+    title: "Area X: Annihilation",
+    year: "2018",
+    genres: ["Adventure", "Drama", "Fantasy"],
+    releaseDate: "2018-02-23",
+    actors: ["Tessa Thompson", "Jennifer Jason Leigh", "Natalie Portman"],
   },
   {
-    id: '3',
-    title: 'Hannah',
-    year: '2017',
-    genres: ['Drama'],
-    releaseDate: '2018-01-24',
-    actors: ['Charlotte Rampling', 'Andr Wilms', 'Phanie Van Vyve'],
+    id: "3",
+    title: "Hannah",
+    year: "2017",
+    genres: ["Drama"],
+    releaseDate: "2018-01-24",
+    actors: ["Charlotte Rampling", "Andr Wilms", "Phanie Van Vyve"],
   },
 ];
 
@@ -31,54 +31,66 @@ Use the variable named movies to solve the requirement below.
 */
 
 // 1. Log the value of the title key in the first movie of the array named `movies`
-
+console.log(movies[0].title);
 // 2. Log the value of the title key in the last movie of the array named `movies`
-
+console.log(movies[2].title);
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
-
+console.log(movies[1].title);
 // 4. Log the value of the year key in the first movie of the array named `movies`
-
+console.log(movies[0].year);
 // 5. Log all the named of actors (key named actors) of the first movie
-
+console.log(movies[0].actors);
 // 6. Log the name of the first actor of the first movie
-
+console.log(movies[0].actors[0]);
 // 7. Log the name of the last actor of the first movie
-
+console.log(movies[0].actors[2]);
 // 8. Log the name of the second actor (index 1) of the first movie
-
+console.log(movies[0].actors[1]);
 // 9. Log the name of the second actor (index 1) of the second movie
-
+console.log(movies[1].actors[1]);
 // 10. Log the name of the last actor of the thrid movie
-
+console.log(movies[2].actors[2]);
 // 11. Log the name of the second actor (index 1) of the third movie
-
+console.log(movies[2].actors[1]);
 // 12. Log all the genres of the third movie
-
+console.log(movies[2].genres);
 // 13. Log all the genres of the first movie
-
+console.log(movies[0].genres);
 // 14. Log the first genres of the first movie
-
+console.log(movies[0].genres[0]);
 // 15. Log the first genres of the second movie
-
+console.log(movies[1].genres[0]);
 // 16. Log the last genres of the first movie (using the length property of array)
-
+// console.log(movies[0].genres[genres.length]);
 // 17. Log the first genres of the second movie (using the length property of array)
 
 // 18. Log all the genres of the first movie one by one
+for (movie of movies) {
+  console.log(movies[0].genres);
+}
 
 // 19. Log all the genres of the second movie one by one
+for (movie of movies) {
+  console.log(movies[1].genres);
+}
 
 // 20. Log if the first actor of the first movie is `Rachel McAdams` or not (You have to log true or false)
+console.log(`Rachel McAdams` in movies[0].actors);
 
 // 21. Log if the second actor (index 1) of the second movie is `Natalie Portman` or not (You have to log true or false)
 
 // 22. Log if the year of all three movies is greater than `2017` or not one by one. (Log true or false)
-
+for (movie of movies) {
+  console.log(2017 in movies);
+}
 // 23. Log the title of all three movies one by one.
+// console.log(movies[movies.length].title);
 
 // 24. Log if the title of the first movie is `Hannah` or not
-
+console.log(`Hannah` in movies[2]);
 // 25. Log the number of actors in all three movies one by one
+
+console.log(movies[1].actors);
 
 // 26. Log the number of genres in all three movies one by one
 
