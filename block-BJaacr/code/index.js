c3ae6cfd5771207865c228adb1ac3119e1f37c65
ritik-cyber <ritikdhingra03@gmail.com>
2021-05-30 10:50:29 +0530

    done

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -19,10 +19,14 @@ Hint: (Structure of got variable)
 */
 
 // 1. Log the value of key "houses"
-
+console.log(got.houses);
 // 2. Log the length of the "houses" key
+console.log(got.houses.length);
 
 // 3. Log the name of all houses in got one by one
+for (em of got.house) {
+  console.log(em);
+}
 
 // 4. Log the name of all the people in house Starks (index 0) one by one
 
